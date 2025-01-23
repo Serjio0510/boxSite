f464a490383d201ed9014438aed9034f68ba2f21
Serjio0510 <komegunovsergej@gmail.com>
2025-01-23 11:26:59 +0300

    First commit

---
+++ sw.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('my-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/src/styles.css',
+          '/src/script.js',
+          'src/img/favicon.png',
+          'src/img/fon_page.png',
+          'src/img/fon.png',
+          'src/img/logo.png',
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
