0c9266efb552a1f8c8a12d030e3a874359b99063
Serjio0510 <komegunovsergej@gmail.com>
2025-01-31 14:09:48 +0300

    service

--- src/script.js
+++ src/script.js
@@ -131,3 +131,14 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+//serviceWorker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/boxSite/sw.js').then((registration) => {
+      console.log('Service Worker зарегистрирован с областью:', registration.scope);
+    }).catch((error) => {
+      console.log('Ошибка регистрации Service Worker:', error);
+    });
+  });
+}
